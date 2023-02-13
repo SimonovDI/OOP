fe988353fe8a5abb3c07d8ba27ab68e1ec2af30f
SimomovDI <denigsim@yandex.ru>
2023-02-13 22:18:00 +0300

    new_commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+class MyDict:
+    keys = None
+    values = None
+    length = None
+    dict = dict()
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def get_item(self):
+        self.dict[self.key] = self.value
+
+
+p1 = MyDict("apple", 15)
+print(p1.__dict__)
