6f3937b863b297d4976e6da79ae90f1e75d90eb2
SimomovDI <denigsim@yandex.ru>
2023-02-14 00:31:18 +0300

    add test

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-class MyDict:
-    keys = None
-    values = None
-    length = None
-    dict = dict()
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    def get_item(self):
-        self.dict[self.key] = self.value
-
-
-p1 = MyDict("apple", 15)
-print(p1.__dict__)
+print("test")
